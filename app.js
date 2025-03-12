90ffadbc7b92c94ba41c0f4856f35de129d44d5d
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2025-03-12 15:30:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 const app = express();
 
@@ -17,6 +17,30 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
-app.listen(80,()=>{
+app.get("/pokemons", async (request, response) => {
+    const result = await pool.query("SELECT * FROM pokemons ORDER BY id");
+    response.json(result.rows);
+});
+
+app.post("/pokemons", async (request, response) => {
+    const { nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2 } = request.body;
+    const result = await pool.query("INSERT INTO pokemons (nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *", [nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2]);
+    response.json(result.rows[0]);
+});
+
+app.put("/pokemons/:id", async (request, response) => {
+    const id = request.params.id;
+    const { nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2 } = request.body;
+    const result = await pool.query("UPDATE pokemons SET nome=$1, peso=$2, ataque=$3, defesa=$4, velocidade=$5, habilidade1=$6, habilidade2=$7 WHERE id=$8 RETURNING *", [nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2, id]);
+    response.json(result.rows[0]);
+});
+
+app.delete("/pokemons/:id", async (request, response) => {
+    const id = request.params.id;
+    const result = await pool.query("DELETE FROM pokemons WHERE id=$1", [id]);
+    response.json(result.rows);
+});
+
+app.listen(80, () => {
     console.log("Server is running on port 80");
 });
